ccf96174a6490bb139926556d0f07db5c723bf38
Atef Chelaghma <atef.chelaghma@sovos.com>
2023-06-23 18:18:46 -0300

    update code

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -38,7 +38,7 @@ function App() {
       className="px-4 py-2 border border-gray-300 rounded-l-md focus:outline-none focus:ring-blue-500 focus:border-blue-500" 
       type="text" 
       placeholder="Filter"
-      // value={inputValue}
+      value={inputValue}
       onChange={(e) => setInputValue(e.target.value)}
       />
      
